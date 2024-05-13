61716b98bed417a66649a6de7c7e106ef4303604
CherazBerkat <142236701+CherazBerkat@users.noreply.github.com>
2024-05-13 14:30:31 +0100

    ortho added meths

--- TP/src/main/java/com/example/tp/Models/Ortho.java
+++ TP/src/main/java/com/example/tp/Models/Ortho.java
@@ -7,19 +7,56 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import static com.example.tp.HelloApplication.*;
 
-public class Ortho extends Person implements  Serializable{
+public class Ortho implements  Serializable{
+    private String nom;
+    private String prenom;
+    private String tel;
     private String mail;
     private String passwd;
     private String userName;
     private ArrayList<SerieExo> serieExos =new ArrayList<>();
     private ArrayList<SerieQuestion> serieQuestions=new ArrayList<>();
+    private  ArrayList<Dossier> dossiers=new ArrayList<>();
+    private ArrayList<Anamnese> anamneses=new ArrayList<>();
+    private ArrayList<RendezVous> rendezVous=new ArrayList<>();
+
+
+
+
+
     public Ortho (String n,String p,String t,String m,String pd){
-        super(n,p,t);
+        nom=n;
+        prenom=p;
+        tel=t;
         mail=m;
         passwd=pd;
     }
     public Ortho(){}
 
+    /**************************************************************************************************/
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
     public void setPasswd(String passwd) {
         this.passwd = passwd;
     }
@@ -43,12 +80,45 @@ public class Ortho extends Person implements  Serializable{
      public String getUserNAme(){
         return userName;
      }
+
+     /***********************************************************************************************/
     public void addSerieExos(SerieExo s){
         serieExos.add(s);
     }
     public void addSerieQuestions(SerieQuestion s){
         serieQuestions.add(s);
     }
+    public void addAnamnese(Anamnese s){
+        anamneses.add(s);
+    }
+    public void addDossier(Dossier s){
+        dossiers.add(s);
+    }
+    public void addRendezVous(RendezVous s){
+        rendezVous.add(s);
+    }
+    public void deleteRendezVousIndx (int index){
+        if (index >= 0 && index < rendezVous.size()) {
+            rendezVous.remove(index);
+        } else {
+            System.out.println("Invalid index. No rendez-vous deleted.");
+        }
+    }
+    public void deleteDossierIndx (int index){
+        if (index >= 0 && index < dossiers.size()) {
+            dossiers.remove(index);
+        } else {
+            System.out.println("Invalid index. No dossier deleted.");
+        }
+    }
+    public void deleteAnamneseIndx (int index){
+        if (index >= 0 && index < anamneses.size()) {
+            anamneses.remove(index);
+        } else {
+            System.out.println("Invalid index. No Anamnese deleted.");
+        }
+    }
+
     public void deleteSerieExosIndx (int index){
         if (index >= 0 && index < serieExos.size()) {
             serieExos.remove(index);
--- TP/src/main/java/com/example/tp/Models/Patient.java
+++ TP/src/main/java/com/example/tp/Models/Patient.java
@@ -1,17 +1,43 @@
 package com.example.tp.Models;
 import java.io.Serializable;
 import java.time.LocalDate;
-public class Patient extends Person implements Serializable {
+public class Patient implements Serializable {
     private LocalDate dateNaissance;
     private String lieuNaissance;
     private int numDoss;
-
+    private String nom ;
+    private String prenom;
+    private String tel;
     public Patient (LocalDate date ,String lieu , String n ,String p, String t){
-        super(n,p,t);
+        nom=n;
+        prenom=p;
+        tel=t;
        dateNaissance = date;
        lieuNaissance=lieu;
     }
+    public String getNom() {
+        return nom;
+    }
 
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
     public void setDateNaissance(LocalDate dateNaissance) {
         this.dateNaissance = dateNaissance;
     }

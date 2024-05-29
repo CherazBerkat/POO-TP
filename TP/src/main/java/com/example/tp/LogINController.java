95f74cb894d635a9c2f84aa30c70173e368a8856
CherazBerkat <142236701+CherazBerkat@users.noreply.github.com>
2024-05-29 07:43:30 +0100

    my final push

--- TP/src/main/java/com/example/tp/LogINController.java
+++ TP/src/main/java/com/example/tp/LogINController.java
@@ -1,12 +1,9 @@
 package com.example.tp;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import java.io.IOException;
 import static com.example.tp.HelloApplication.orthophonist;
